df24541c36de30014db1bf26d0ad36954250631c
Daniel <romerodaniel@protonmail.com>
2023-01-23 17:58:11 +0100

    Started the look and feel part of the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  googlemapsapi: 'AIzaSyBG1GTbL9qAl8ZZIMt0k1S0VOf4DxMAVKM',
   supabaseUrl: 'https://jvavmszzitymsnzrvhth.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp2YXZtc3p6aXR5bXNuenJ2aHRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE1NjIwMTQsImV4cCI6MTk4NzEzODAxNH0.o3vYsZ5_hUFM2lpX3jgx_1IwILxhUfok3kAzjzPgz2A',
 };
